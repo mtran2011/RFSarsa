80340e68d5674bf990a9993cf6bf317116e9d741
Student <tranhaminh@gmail.com>
2018-01-07 22:51:18 -0500

    update

--- source/main.py
+++ source/main.py
@@ -1,22 +1,19 @@
 import itertools
 from math import log
+import pandas as pd
 import matplotlib.pyplot as plt
 from stock import OULogStock
 from exchange import StockExchange
 from trader import TabularQMatrixStockTrader, TabularSarsaStockTrader, RFSarsaStockTrader
 from environment import StockTradingEnvironment
 
-def graph_performance(performance_dict: dict, ntrain: int, version: int):
+def graph_performance(df: pd.DataFrame, ntrain: int, version: int):
     linestyles = itertools.cycle(['-', ':', '--', '-.'])
     colors = itertools.cycle(['b', 'g', 'r', 'y', 'k', 'm', 'c'])
     plt.figure()
-    ntest = None
-    for name, performance in performance_dict.items():
-        if ntest is None:
-            ntest = len(performance)-1
-        else:
-            assert ntest == len(performance)-1
-        plt.plot(range(len(performance)), performance, label=name, linestyle=next(linestyles), color=next(colors))
+    for name in df.columns:
+        plt.plot(df.index, df[name], label=name, linestyle=next(linestyles), color=next(colors))
+    ntest = len(df.index)-1
     plt.title('Performance with ntrain = {0:,} and ntest = {1:,}'.format(ntrain, ntest))
     plt.legend(loc='best')
     plt.xlabel('iterations in the test run')
@@ -29,7 +26,7 @@ def main():
     lot = 10
     actions = tuple(range(-5*lot, 6*lot, lot))
     stock_exchange = StockExchange(oustock, lot, tick=1, max_holding=100*lot)
-    utility, ntrain, ntest = 1e-3, 1000, 500
+    utility, ntrain, ntest = 1e-3, 100, 100
     epsilon, learning_rate, discount_factor = 0.1, 0.5, 0.999
     tabular_qmatrix = TabularQMatrixStockTrader('tabular q-learning', utility, stock_exchange, actions, epsilon, learning_rate, discount_factor)
     tabular_sarsa = TabularSarsaStockTrader('tabular sarsa', utility, stock_exchange, actions, epsilon, learning_rate, discount_factor)
@@ -37,6 +34,8 @@ def main():
     trading_environment = StockTradingEnvironment(stock_exchange)
     trading_environment.run(ntrain)
     result = trading_environment.run(ntest, report=True)
+    result = pd.DataFrame(result)
+    result.sort_index(axis=1, inplace=True)
     graph_performance(result, ntrain, version=0)
 
 if __name__ == '__main__':
